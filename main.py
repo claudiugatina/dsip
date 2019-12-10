cc29013c9ed04221e065641d6ade2071730abbb2
Claudiu <claudiu.gatina@gmail.com>
2019-12-10 18:43:35 +0200

    Can plot matrix

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+import matplotlib.pyplot as plt
+import numpy as np
 
+x, y = np.meshgrid(np.linspace(0,255,255), np.linspace(0,255,255))
+z = x * y
 
-print("Hello world")
+for i in range(0, 255):
+    z[155][i] = 0
+
+# Plot the grid
+plt.imshow(z)
+plt.gray()
+plt.show()
